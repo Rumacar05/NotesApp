a6dc5ed2c0f1c725c65422353a119587de496d84
Rumacar05 <Rumacar05@gmail.com>
2025-03-05 09:50:27 +0100

    Add theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.ruma.notes"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ruma.notes"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
