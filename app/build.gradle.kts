dff923ada1783e368146011f115b4036cc93b736
Rumacar05 <Rumacar05@gmail.com>
2025-03-11 11:21:45 +0100

    Update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.google.devtools.ksp")
-    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.devtools.ksp)
+    alias(libs.plugins.dagger.hilt.android)
     id("kotlin-kapt")
 }
 
